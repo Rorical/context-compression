45c39f3139ddb4fef77f86ea611f3401c769962b
Rorical <46294886+Rorical@users.noreply.github.com>
2026-04-01 15:48:13 +0800

    Add context_compression/__init__.py

---
+++ context_compression/__init__.py
@@ -0,0 +1,21 @@
+"""Context compression training package."""
+
+from .data_pipeline import DataPipeline
+from .evaluator import ContextCompressionEvaluator
+from .models import ModelInference, ModelLoader, load_model_for_inference, load_model_for_training
+from .trainer import ContextCompressionTrainer
+from .utils import get_config_for_gpu, get_default_config, load_config, save_config
+
+__all__ = [
+    "ContextCompressionEvaluator",
+    "ContextCompressionTrainer",
+    "DataPipeline",
+    "ModelInference",
+    "ModelLoader",
+    "get_config_for_gpu",
+    "get_default_config",
+    "load_config",
+    "load_model_for_inference",
+    "load_model_for_training",
+    "save_config",
+]
